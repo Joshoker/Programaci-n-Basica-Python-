ab316e3cf28a1d247fb04bfe9aa97e7425c5b6d8
Joshoker <joshua.nava@gmail.com>
2022-09-28 17:17:45 -0500

    Adición de programas
    Se añadieron los programas restantes.

--- Estructuras de control/Iteraciones (for-while).py
+++ Estructuras de control/Iteraciones (for-while).py
@@ -1,10 +1,3 @@
-# El usuario ingresa el límite de datos recopilados 
-limit = int(input("Ingresa un número mayor a 3")) 
-# Muestra los números positivos en múltiplos de 3 
-print("los multiplos de tres", limit, "son:") 
-for i in range(3, limit + 1, 3): 
-    print(i)
-
 MIN = 1 
 MAX = 20 
 #La instrucción end="" al final del argumento de print, evita que el programa continue a la siguiente línea 
